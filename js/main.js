deea0666129f848ca4acdf3c2c7e063c9d54537f
BryantGG77 <brayespro@gmail.com>
2025-01-24 11:42:49 -0500

    Agregando Scroll y indicador de scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+// Variables formulario
+
 const form = document.querySelector('.contacto__form');
 const formBtn = document.querySelector('.contacto__form__button');
+
+// Variables botón ir arriba e indicador de scroll
+
+const boton_ir_arriba = document.querySelector('#ir_arriba_boton');
+const barra_indicador = document.querySelector('.indicador_scroll');
+const obtener_pixeles_inicio = () => document.documentElement.scrollTop || document.body.scrollTop;
+
+// Manejo del formulario
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -18,3 +28,28 @@ form.addEventListener('submit', (e) => {
 
     form.reset();
 });
+
+
+// Manejo del botón ir arriba e indicador
+
+const irArriba = () => {
+    if (obtener_pixeles_inicio() > 0) {
+        scrollTo(0, 0);
+    }
+}
+
+const indicarScroll = () => {
+    if (obtener_pixeles_inicio() > 50) {
+        boton_ir_arriba.className = 'mostrar'
+    } else {
+        boton_ir_arriba.className = 'ocultar'
+    }
+
+    let alto = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let avance_scroll = (obtener_pixeles_inicio() / alto) * 100;
+    barra_indicador.style.width = `${avance_scroll}%`;
+}
+
+boton_ir_arriba.addEventListener('click', irArriba);
+
+window.addEventListener('scroll', indicarScroll);
